ae6d2e452b45af0a3beb33840487ca8590585c8a
David Padilla <david@crowdint.com>
2013-02-23 13:15:00 -0600

    Changes for Bootstrap 2.3.0

--- lib/bootstrap-form/form_helper.rb
+++ lib/bootstrap-form/form_helper.rb
@@ -5,41 +5,41 @@ module  ActionView
       BOOTSTRAP_OPTIONS = [:label, :hint].freeze
 
       def bootstrap_text_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, text_field(object_name, method, extract_input_options(options)), options)
+        bootstrap_control_group_wrap(object_name, method, text_field(object_name, method, extract_input_options(options)), options)
       end
 
       def bootstrap_email_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, email_field(object_name, method, extract_input_options(options)), options)
+        bootstrap_control_group_wrap(object_name, method, email_field(object_name, method, extract_input_options(options)), options)
       end
 
       def bootstrap_password_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, password_field(object_name, method, extract_input_options(options)), options)
+        bootstrap_control_group_wrap(object_name, method, password_field(object_name, method, extract_input_options(options)), options)
       end
 
       def bootstrap_collection_select(object_name, method, collection, value_method, text_method, options = {}, html_options = {})
-        bootstrap_clearfix_wrap(object_name, method, collection_select(object_name, method, collection, value_method, text_method, extract_input_options(options), html_options), options)
+        bootstrap_control_group_wrap(object_name, method, collection_select(object_name, method, collection, value_method, text_method, extract_input_options(options), html_options), options)
       end
 
       def bootstrap_select(object_name, method, choices, options, html_options)
-        bootstrap_clearfix_wrap(object_name, method, select(object_name, method, choices, extract_input_options(options), html_options), options)
+        bootstrap_control_group_wrap(object_name, method, select(object_name, method, choices, extract_input_options(options), html_options), options)
       end
 
       def bootstrap_file_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, file_field(object_name, method, extract_input_options(options)), options)
+        bootstrap_control_group_wrap(object_name, method, file_field(object_name, method, extract_input_options(options)), options)
       end
 
       def bootstrap_text_area(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, text_area(object_name, method, extract_input_options(options)), options)
+        bootstrap_control_group_wrap(object_name, method, text_area(object_name, method, extract_input_options(options)), options)
       end
 
-      def bootstrap_clearfix_wrap(object_name, method, content, options={})
+      def bootstrap_control_group_wrap(object_name, method, content, options={})
         error_messages = options[:object].errors[method]
-        clearfix_tag = error_messages.blank? ? 'clearfix' : 'clearfix error'
-        inline_help = inline_help_tag(error_messages.presence || options[:hint])
+        control_group_tag   = error_messages.blank? ? 'control-group' : 'control-group error'
+        inline_help    = inline_help_tag(error_messages.presence || options[:hint])
 
-        content_tag(:div, label(object_name, method, options[:label]) +
-            content_tag(:div, content + inline_help, :class => 'input'),
-            :class => clearfix_tag)
+        content_tag(:div, label(object_name, method, options[:label], :class => 'control-label') +
+            content_tag(:div, content + inline_help, :class => 'controls'),
+            :class => control_group_tag)
       end
 
       def inline_help_tag(messages)
--- test/lib/action_view/helpers/form_helper_test.rb
+++ test/lib/action_view/helpers/form_helper_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class FormHelperTest < ActionView::TestCase
-  def test_bootstrap_clearfix_wrap
+  def test_bootstrap_control_group_wrap
     object  = mock
     errors  = { :name   => [] }
     options = { :object => object }
@@ -9,11 +9,11 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input">content</div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group"><label class="control-label" for="post_name">Name</label><div class="controls">content</div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
-  def test_bootstrap_clearfix_wrap_with_label
+  def test_bootstrap_control_group_wrap_with_label
     object  = mock
     errors  = { :name   => [] }
     options = { :object => object, :label => "Custom" }
@@ -21,11 +21,11 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix"><label for="post_name">Custom</label><div class="input">content</div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group"><label class="control-label" for="post_name">Custom</label><div class="controls">content</div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
-  def test_bootstrap_clearfix_wrap_with_errors
+  def test_bootstrap_control_group_wrap_with_errors
     object  = mock
     errors  = { :name   => ["can't be blank"] }
     options = { :object => object }
@@ -33,11 +33,11 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input">content<span class="help-inline"> can't be blank</span></div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group error"><label class="control-label" for="post_name">Name</label><div class="controls">content<span class="help-inline"> can't be blank</span></div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
-  def test_bootstrap_clearfix_wrap_with_many_errors
+  def test_bootstrap_control_group_wrap_with_many_errors
     object  = mock
     errors  = { :name   => ["has already been taken", "is reserved", "must be odd"] }
     options = { :object => object }
@@ -45,11 +45,11 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input">content<span class="help-inline"> has already been taken, is reserved, and must be odd</span></div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group error"><label class="control-label" for="post_name">Name</label><div class="controls">content<span class="help-inline"> has already been taken, is reserved, and must be odd</span></div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
-  def test_bootstrap_clearfix_wrap_with_hint
+  def test_bootstrap_control_group_wrap_with_hint
     object  = mock
     errors  = {}
     options = { :object => object, :hint => "format matters" }
@@ -57,11 +57,11 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input">content<span class="help-inline"> format matters</span></div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group"><label class="control-label" for="post_name">Name</label><div class="controls">content<span class="help-inline"> format matters</span></div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
-  def test_bootstrap_clearfix_wrap_with_hint_and_errors
+  def test_bootstrap_control_group_wrap_with_hint_and_errors
     object  = mock
     errors  = { :name   => ["can't be blank"] }
     options = { :object => object, :hint => "format matters" }
@@ -69,8 +69,8 @@ class FormHelperTest < ActionView::TestCase
     stub(object).errors { errors }
     stub(object).name   { 'Object Name' }
 
-    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input">content<span class="help-inline"> can't be blank</span></div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
+    expected_code = %{<div class="control-group error"><label class="control-label" for="post_name">Name</label><div class="controls">content<span class="help-inline"> can't be blank</span></div></div>}
+    assert_equal expected_code, bootstrap_control_group_wrap(:post, :name, content, options)
   end
 
   def test_bootstrap_text_field
@@ -78,7 +78,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).text_field(:post, :name, options) { text_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :name, text_field, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :name, text_field, options.dup) { html }
     assert_equal html, bootstrap_text_field(:post, :name, options)
   end
 
@@ -87,7 +87,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).email_field(:post, :email, options) { email_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :email, email_field, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :email, email_field, options.dup) { html }
     assert_equal html, bootstrap_email_field(:post, :email, options)
   end
 
@@ -96,7 +96,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).password_field(:post, :password, options) { password_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :password, password_field, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :password, password_field, options.dup) { html }
     assert_equal html, bootstrap_password_field(:post, :password, options)
   end
 
@@ -105,7 +105,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).collection_select(:post, :name, collection, :id, :name, options, {}) { collection_select_html }
-    mock(self).bootstrap_clearfix_wrap(:post, :name, collection_select_html, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :name, collection_select_html, options.dup) { html }
 
     assert_equal html, bootstrap_collection_select(:post, :name, collection, :id ,:name, options)
   end
@@ -115,7 +115,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).select(:post, :name, choices, options, {}) { select_html }
-    mock(self).bootstrap_clearfix_wrap(:post, :name, select_html, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :name, select_html, options.dup) { html }
 
     assert_equal html, bootstrap_select(:post, :name, choices, options, {})
   end
@@ -125,7 +125,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).file_field(:post, :attachment, options) { text_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :attachment, text_field, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :attachment, text_field, options.dup) { html }
     assert_equal html, bootstrap_file_field(:post, :attachment, options)
   end
 
@@ -134,7 +134,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).text_area(:post, :description, options) { text_area }
-    mock(self).bootstrap_clearfix_wrap(:post, :description, text_area, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :description, text_area, options.dup) { html }
     assert_equal html, bootstrap_text_area(:post, :description, options)
   end
 
@@ -143,7 +143,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock, :label => "Custom", :hint => "be careful" }
 
     mock(self).text_area(:post, :description, options.except(:label, :hint)) { text_area }
-    mock(self).bootstrap_clearfix_wrap(:post, :description, text_area, options.dup) { html }
+    mock(self).bootstrap_control_group_wrap(:post, :description, text_area, options.dup) { html }
     assert_equal html, bootstrap_text_area(:post, :description, options)
   end
 
